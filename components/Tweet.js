0f6428ef03065092484c03e1bb0c19543440c999
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-10-17 19:01:58 -0600

    icons work

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,31 +1,49 @@
-import React from 'react'
-import {Text, View, StyleSheet, Image} from 'react-native'
+import React from 'react';
+import {Text, View, StyleSheet, Image, Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const heart = <Icon name="heart-o" size={20} color="#000" />;
+const retweet = <Icon name="retweet" size={20} color="#000" />;
+const comment = <Icon name="comment-o" size={20} color="#000" />;
+const share = <Icon name="share" size={20} color="#000" />;
 
-export default Tweet=(props)=>{
-    return(
-        <View style={styles.tweetContain}>
-            <Image
-            style={styles.img}
-            source={{uri:props.photoUrl}}/>
-            <View>
-                <Text>{`${props.accName} ${props.handle}`}</Text>
-                <Text>{props.tweetContent}</Text>
-                <View><Text>Icons</Text></View>
-                </View>    
+export default Tweet = props => {
+  return (
+    <View style={styles.tweetContain}>
+      <Image style={styles.img} source={{uri: props.photoUrl}} />
+      <View>
+        <Text>{`${props.accName} ${props.handle}`}</Text>
+        <Text>{props.tweetContent}</Text>
+        <View style={styles.iconHolder}>
+          {heart}
+          {share}
+          {comment}
+          {retweet}
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
-const styles=StyleSheet.create({
-    tweetContain: {
-        display:'flex', 
-        flexDirection:'row', 
-        justifyContent:'center', 
-        alignItems:'flex-start'
-    }, 
-    img: {
-      height:25, 
-      width:25, 
-      borderRadius:25  
-    }
-})
+const styles = StyleSheet.create({
+  tweetContain: {
+    // padding:5,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    marginLeft: Dimensions.get('screen').width * 0.05,
+    width: Dimensions.get('screen').width * 0.9,
+    marginBottom: 5,
+  },
+  img: {
+    height: 45,
+    width: 45,
+    borderRadius: 25,
+  },
+  iconHolder: {
+      display:'flex', 
+      flexDirection:'row', 
+      justifyContent:'space-evenly', 
+      alignItems:'center'
+  }
+});
